d3749f1b58a361db26b1e329e37fe6480fb39d12
khaled hesham <kha.hesham@gmail.com>
2023-02-17 08:40:14 +0200

    day 2 leet code

---
+++ searchRange.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+
+    
+    vector<int> searchRange(vector<int>& nums, int target) {
+
+        int start = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        int end = lower_bound(nums.begin(), nums.end(), target+1) - nums.begin() - 1;
+        if(start < nums.size() && nums[start] == target)
+            return {start, end};
+        else 
+            return {-1,-1};
+        
+    
+    }
+};
