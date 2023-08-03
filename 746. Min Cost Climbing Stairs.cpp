b66ba70be0f9dda070788a480a2d684cc3687bb7
khaled hesham <kha.hesham@gmail.com>
2023-08-03 09:45:59 +0300

    # 3 keep going man

--- 746. Min Cost Climbing Stairs.cpp
+++ 746. Min Cost Climbing Stairs.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
-int recursive(int n,vector<int>& cost,vector<int> &memo)
-    {
-        if(n < 0) return 0;
-        if(n == 0 || n==2 ) 
-            return cost[n];
-
-        if(memo[n] == -1) 
-            memo[n] = cost[n] + min(recursive(n-1,cost,memo), recursive(n-2,cost,memo));
-
-        return memo[n];
-    }
     
     int minCostClimbingStairs(vector<int>& cost) {
         
-        vector<int> memo(cost.size(),-1);
-        return min(recursive(cost.size()-1,cost,memo),recursive(cost.size()-2,cost,memo));
+        int n = cost.size();
+        for(int i=2;i<n;i++)
+        {
+            cost[i]+=min(cost[i-1],cost[i-2]);
+        }
+        return min(cost[n-1],cost[n-2]);
     }
 };
