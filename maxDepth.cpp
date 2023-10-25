c174a0efa5dd0e5a89cdca4c8a6fa565afc4f35e
khaled hesham <kha.hesham@gmail.com>
2023-10-25 11:55:49 +0300

    maxDepth of BST

---
+++ maxDepth.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+
+        int L = 1 + maxDepth(root->right);
+        int R = 1 + maxDepth(root->left);
+
+        return max(L,R);
+
+    }
+};
