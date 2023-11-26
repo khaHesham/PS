4db226f89e3d3a2c265618357bf9328771506671
khaled hesham <kha.hesham@gmail.com>
2023-11-26 10:12:53 +0200

    adding bottom up solution

--- Assignment 3/min Edit distance.cpp
+++ Assignment 3/min Edit distance.cpp
@@ -6,6 +6,32 @@ public:
 
 
 
+    int BottomUp(string word1, string word2){
+            vector<vector<int>> memo(word1.length() + 1,vector<int>(word2.length() + 1,0));
+
+            // initialize the base case
+            for(int i = 0;i < word1.length()+ 1;i++){
+                memo[i][0] = i;
+            }
+            for (int j = 0; j < word2.length() + 1; j++) {
+                memo[0][j] = j;
+            }
+
+            // algorithm
+            for(int i = 1; i< word1.length() + 1; i++)
+            {
+                for(int j = 1; j < word2.length() + 1; j++)
+                {
+                    if(word1[i-1]==word2[j-1]) memo[i][j] = memo[i-1][j-1];
+                    else{
+                        // find min in memo[i-1][j], memo[i][j-1], memo[i-1][j-1]
+                        memo[i][j] = 1 + min(memo[i-1][j],min(memo[i][j-1],memo[i-1][j-1]));
+                    }
+                }
+            }
+            return memo[word1.length()][word2.length()];
+        }
+
     int solve(string word1,string word2, int i,int j, vector<vector<int>>& memo) {
 
         if (i == word1.length() && j == word2.length()) return 0;
@@ -24,7 +50,7 @@ public:
     }
     int minDistance(string word1, string word2) {
         vector<vector<int>> memo(word1.length() + 1,vector<int>(word2.length() + 1,-1));
-        return solve(word1,word2,0,0,memo);
+        return BottomUp(word1,word2);
     }
 };
 
